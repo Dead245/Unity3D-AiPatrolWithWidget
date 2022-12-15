1613408399e7d736fa3f4cb963d8dce140ba0e8a
Dead245 <116427900+Dead245@users.noreply.github.com>
2022-12-14 19:23:06 -0600

    Created update method for object to traverse

--- AI Patrol with Widget/Assets/PatrolManager.cs
+++ AI Patrol with Widget/Assets/PatrolManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,22 +9,31 @@ public class PatrolManager : MonoBehaviour
 
     [SerializeField] GameObject traversingObject;
     [SerializeField] bool shouldLoop;
-
-
+    Animator objectAnim;
+    float walkSpeed = 2f;
+    int currentTarget = 0;
     //Gizmos must be turned on for the positions to be updated as you move the gameObjects for the points in the scene.
     [SerializeField]
     List<Point> points = new List<Point>();
 
-    void Start()
-    {
+    void Start() {
+        objectAnim = traversingObject.GetComponent<Animator>();
+    }
+    void Update() {
+        if (Vector3.Distance(traversingObject.transform.position, points[currentTarget].position) < 0.2f && currentTarget != points.Count - 1) {
+            currentTarget++;
+        }
+
+        if (Vector3.Distance(traversingObject.transform.position, points[currentTarget].position) > 0.2f){
+            traversingObject.transform.LookAt(points[currentTarget].position, Vector3.up);
+            traversingObject.transform.position = Vector3.MoveTowards(traversingObject.transform.position, points[currentTarget].position, walkSpeed * Time.deltaTime);
+            objectAnim.SetBool("shouldMove", true);
+            Debug.Log("Distance: " + Vector3.Distance(traversingObject.transform.position, points[currentTarget].position) + "  |Moving to point #" + currentTarget);
+        }
 
     }
-    void Update()
-    {
-        
-    }
 
-
+    //Creates point when you click on the "Add Point" button on the component in the editor (requires MovementManagerEditor)
     public void GeneratePoint() {
         GameObject point = Instantiate(transform.GetChild(0).gameObject,transform.GetChild(transform.childCount - 1).position, Quaternion.identity, transform);
         point.name = "Move Point #" + transform.childCount;
@@ -33,40 +43,38 @@ public class PatrolManager : MonoBehaviour
     }
 
     //Draws lines between the points of the path and wire spheres on the points while in the editor
-    private void OnDrawGizmos()
-    {
+    private void OnDrawGizmos() {
         //Draws lines between children of what GameObject this script is on
         Gizmos.color = Color.white;
-        Vector3 firstPoint = gameObject.transform.GetChild(0).position;
-        for (int i = 1; i < gameObject.transform.childCount; i++) { 
-            Vector3 secondPoint = gameObject.transform.GetChild(i).position;
+        Vector3 firstPoint = transform.GetChild(0).position;
+        for (int i = 1; i < transform.childCount; i++) { 
+            Vector3 secondPoint = transform.GetChild(i).position;
             Gizmos.DrawLine(firstPoint, secondPoint);
             firstPoint = secondPoint; 
         }
         if (shouldLoop) {
-            Gizmos.DrawLine(gameObject.transform.GetChild(0).position, gameObject.transform.GetChild(gameObject.transform.childCount-1).position);
+            Gizmos.DrawLine(transform.GetChild(0).position, transform.GetChild(transform.childCount-1).position);
         }
 
         //Adds small wire sphere at each point with label showing its number in the hierarchy
-        for (int i = 0; i < gameObject.transform.childCount; i++) {
+        for (int i = 0; i < transform.childCount; i++) {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(gameObject.transform.GetChild(i).position, 0.1f);
+            Gizmos.DrawWireSphere(transform.GetChild(i).position, 0.1f);
             
-            Handles.Label(gameObject.transform.GetChild(i).position + Vector3.up*0.5f,(i+1).ToString());
+            Handles.Label(transform.GetChild(i).position + Vector3.up*0.5f,(i+1).ToString());
 
             //Additionally updates the position of the point relating to the gameobject if it has changed
-            if (gameObject.transform.GetChild(i).transform.hasChanged && points.Count > 0) {
-                points[i].position = gameObject.transform.GetChild(i).position;
-                points[i].forward = gameObject.transform.GetChild(i).forward;
-                gameObject.transform.GetChild(i).transform.hasChanged = false;
+            if (transform.GetChild(i).transform.hasChanged && points.Count > 0) {
+                points[i].position = transform.GetChild(i).position;
+                points[i].forward = transform.GetChild(i).forward;
+                transform.GetChild(i).transform.hasChanged = false;
             }
         }
     }
 }
 
 [Serializable]
-public class Point
-{
+public class Point {
     //position is relative to the parent
     public Vector3 position;
     public Vector3 forward;
